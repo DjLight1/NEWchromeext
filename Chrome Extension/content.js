f94d50e42ca22e18da2e7f6b798bfefa3bc6cf74
DjLight1 <travale.houston@speeracademy.org>
2017-05-31 08:54:32 -0500

    Popup v1.0.0.0

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -8,7 +8,7 @@ console.log(firstHref);
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      var firstHref = $("a[href^='http']").eq(0).attr("href"); 
 
       console.log(firstHref);
         // This line is new!
